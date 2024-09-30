61991ca6679c6e6e69921e5f4269b4ade5ce0291
vk-commits <yadavvivek114433@gmail.com>
2024-09-29 22:01:34 -0500

    start, stop process

--- api.py
+++ api.py
@@ -1,10 +1,24 @@
 from flask import Flask
+import subprocess
 
 app = Flask(__name__)
+process = None
 
-@app.route('/')
-def hello():
-    return 'Hello, World!', 200
+@app.route('/start')
+def start():
+    global process
+    process = subprocess.Popen(['python', 'main.py'])
+    return 'Process started', 200
+
+@app.route('/stop')
+def stop():
+    global process
+    if process:
+        process.terminate()
+        process = None
+        return 'Process stopped', 200
+    else:
+        return 'No process running', 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, ssl_context=('cert.pem', 'key.pem'))
